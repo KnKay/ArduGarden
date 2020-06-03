46bafd2846cef2116c54bcc66d425da9d718e37a
Kay Richter <kay.richter@varoenergy.com>
2020-06-03 19:53:04 +0200

    Implemented simple test for dht. Seem to work

--- lib/sensors/DHT_sensor.h
+++ lib/sensors/DHT_sensor.h
@@ -3,12 +3,21 @@
 
 
 #include <hal.h>
+#include <sensors/temperature.h>
+#include <sensors/humidity.h>
+#include <DHT.h>
 
 namespace dhtsensor{
     //A DHT has 2 channels. This means we need to have 2 signals
     class dht_sensor: public hal::multi_signal{
         public: 
+            dht_sensor(byte pin, byte type);
             virtual void update() override;
+            hal::sensors::temperature * temperature; 
+            hal::sensors::humidity * humidity;         
+        
+        protected:
+            DHT * sensor = 0; 
 
     }; //END DHT
 }
