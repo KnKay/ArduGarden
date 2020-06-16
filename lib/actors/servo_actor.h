db56f62390344f22c6c9b5a2758fe55df1134e62
Kay Richter <kay.richter@varoenergy.com>
2020-06-16 20:38:57 +0200

    Added simple servo writer

--- lib/actors/servo_actor.h
+++ lib/actors/servo_actor.h
@@ -3,15 +3,16 @@
 
 #include <value_pin.h>
 #include <Arduino.h>
+#include <Servo.h>
 
 namespace actors{
     //A Servo is dead simply. It can take a valu and set the oputput
     class servo_actor: public hal::value::value_output{
         public:
-           
+           servo_actor(byte a_pin);
         protected:
-            
-
+           virtual void trigger(int value) override; 
+            Servo myservo; 
     };
 };
 
